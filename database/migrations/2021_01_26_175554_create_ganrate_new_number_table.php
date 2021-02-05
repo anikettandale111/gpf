3b266d0311204ba0fc815616fab8620325949d92
pravinyeole <pravinyeole16@gmail.com>
2021-02-05 13:13:28 +0530

    migration ganarate and application table and model

---
+++ database/migrations/2021_01_26_175554_create_ganrate_new_number_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGanrateNewNumberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('ganrate_new_number');
+        Schema::create('ganrate_new_number', function (Blueprint $table) {
+            $table->id();
+             $table->string('gpf_no')->nullable();
+             $table->integer('employee_id')->nullable();
+             $table->string('inital_letter')->nullable();
+             $table->integer('classification_id')->nullable();
+             $table->integer('taluka_id')->nullable();
+             $table->integer('department_id')->nullable();
+             $table->string('employee_name')->nullable();
+             $table->string('date_of_birth')->nullable();
+             $table->integer('designation_id')->nullable();
+             $table->string('joining_date')->nullable();
+             $table->string('retirement_date')->nullable();
+             $table->string('retritment_reason')->nullable();
+             $table->string('total_service')->nullable();
+             $table->string('profile_photo')->nullable();
+             $table->string('c_v_letter')->nullable();
+             $table->integer('bank_id')->nullable();
+             $table->string('bank_account_no')->nullable();
+             $table->string('branch_location')->nullable();
+             $table->string('ifsc_code')->nullable();
+             $table->boolean('gpf_no_status')->default(1)->comment('0 No,1 Yes');
+             $table->boolean('is_active')->default(1)->comment('0 Inactive,1 Active');
+             $table->boolean('is_delete')->default(1)->comment('0 deleted,1 not-deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ganrate_new_number');
+    }
+}
