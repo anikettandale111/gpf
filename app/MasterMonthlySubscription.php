ce89c1ec6439d98e254843eb92521aa55a82b2ca
anikettandale111 <anikettandale111@gmail.com>
2021-03-24 10:58:37 +0530

    Chalan Subscriptiopn bug resolved

--- app/MasterMonthlySubscription.php
+++ app/MasterMonthlySubscription.php
@@ -8,5 +8,5 @@ class MasterMonthlySubscription extends Model
 {
     protected $table = 'master_emp_monthly_contribution_two';
     protected $fillable = [ "gpf_number", "classification_id", "taluka_id", "challan_id", "challan_number", "emc_month", "emc_year",
-                            "emc_emp_id", "emc_desg_id", "emc_dept_id", "monthly_contrubition", "monthly_received", "loan_installment", "monthly_other", "loan_amonut"];
+                            "emc_emp_id", "emc_desg_id", "emc_dept_id", "monthly_contrubition", "monthly_received", "loan_installment", "monthly_other", "loan_amonut","remark","is_active"];
 }
