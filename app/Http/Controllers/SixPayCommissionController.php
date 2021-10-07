d2c379ef26219654086f684e02662f06fc42a8a9
Amar Cdat <amar@cdat.co.in>
2021-10-07 17:23:52 +0530

    update search

--- app/Http/Controllers/SixPayCommissionController.php
+++ app/Http/Controllers/SixPayCommissionController.php
@@ -43,29 +43,97 @@ class SixPayCommissionController extends Controller
       return $next($request);
     });
   }
-  public function index()
+  public function index(Request $request)
   {
     $data['designation'] = Designation::all();
     $data['department'] = Department::all();
     $data['month']=month::orderBy('order_by')->get();
     $data['taluka'] = Taluka::all();
-    $deposits=DB::table('master_vetan_ayog_received AS va')
-    ->leftJoin('master_employee AS me','va.GPFNo','me.gpf_no')
-    ->leftJoin('master_month AS mm','va.Mnt','mm.id')
-    ->select('va.*','me.employee_name','mm.month_name_mar')
-    ->where("va.Year", Session::get('from_year'))
-    ->where("va.Mnt", ">=", 4)
-    ->where("va.pay_number",6);
-    $deposits_two=DB::table('master_vetan_ayog_received AS va')
-    ->leftJoin('master_employee AS me','va.GPFNo','me.gpf_no')
-    ->leftJoin('master_month AS mm','va.Mnt','mm.id')
-    ->select('va.*','me.employee_name','mm.month_name_mar')
-    ->where("va.Year", Session::get('to_year'))
-    ->where("va.Mnt", "<=", 3)
-    ->where("va.pay_number",6)
-    ->union($deposits)
-    ->get();
-    $data['sixpayentry'] = $deposits_two;
+   
+    if($request->ajax()){
+
+      $employeeGpfNo_search = $request->employeeGpfNo_search;
+      $employee_name_search = $request->employee_name_search;
+      $taluka_search = $request->taluka_search;
+      $employee_department_search = $request->employee_department_search;
+      $chalan_taluka_search= $request->chalan_taluka_search;
+      $chalan_serial_no_search= $request->chalan_serial_no_search;    
+
+
+        $deposits=DB::table('master_vetan_ayog_received AS va')
+        ->join('master_employee AS me','va.GPFNo','me.gpf_no')
+        ->join('master_month AS mm','va.Mnt','mm.id')
+        ->join("tbl_monthly_total_chalan","tbl_monthly_total_chalan.id","va.ChallanNo")
+        ->select('va.*','me.employee_name','mm.month_name_mar')
+        ->where("va.Year", Session::get('from_year'))
+        ->where("va.Mnt", ">=", 4)
+        ->where("va.pay_number",6);
+
+    
+
+        $deposits_two=DB::table('master_vetan_ayog_received AS va')
+        ->join('master_employee AS me','va.GPFNo','me.gpf_no')
+        ->join('master_month AS mm','va.Mnt','mm.id')
+        ->join("tbl_monthly_total_chalan","tbl_monthly_total_chalan.id","va.ChallanNo")
+        ->select('va.*','me.employee_name','mm.month_name_mar')
+        ->where("va.Year", Session::get('to_year'))
+        ->where("va.Mnt", "<=", 3)
+        ->where("va.pay_number",6);
+
+          if ($employeeGpfNo_search) {
+              $deposits->where("va.GPFNo", '=', "{$employeeGpfNo_search}");
+              $deposits_two->where("va.GPFNo", '=', "{$employeeGpfNo_search}");
+          }
+          if ($employee_name_search) {
+            $deposits->where("me.employee_name", 'like', "%{$chalan_date_search}%");
+            $deposits_two->where("me.employee_name", 'like', "%{$chalan_date_search}%");
+          }
+          if ($taluka_search) {
+            $deposits->where("me.taluka_id", '=', "{$taluka_search}");
+            $deposits_two->where("me.taluka_id", '=', "{$taluka_search}");
+          }
+          if ($employee_department_search) {
+            $deposits->where("me.department_id", '=', "{$employee_department_search}");
+            $deposits_two->where("me.department_id", '=', "{$employee_department_search}");
+          }
+          if ($chalan_taluka_search) {
+            $deposits->where("tbl_monthly_total_chalan.taluka", '=', "{$chalan_taluka_search}");
+            $deposits_two->where("tbl_monthly_total_chalan.taluka", '=', "{$chalan_taluka_search}");
+          }
+
+      if ($chalan_serial_no_search) {
+        $deposits->where("tbl_monthly_total_chalan.chalan_serial_no", '=', "{$chalan_serial_no_search}");
+        $deposits_two->where("tbl_monthly_total_chalan.chalan_serial_no", '=', "{$chalan_serial_no_search}");
+      }
+
+      $deposits_three = $deposits_two->union($deposits)
+      ->get();
+
+      return datatables()->of($deposits_three)
+      ->addIndexColumn()
+      
+      ->addColumn('action', function ($row) {
+        $btn="";
+        
+          
+        
+          $btn = '<button class="btn btn-danger btn-flat btn-sm remove-user" data-id="'.$row->TransId .'" data-action="'.url('vetan_Delete',$row->TransId).'" onclick="deleteConfirmation(\''.$row->TransId.'\')">
+                            <i class="fa fa-trash"></i>
+                          </button>';
+        /* if(Auth::user()->role_id==2 && $row->is_active==0 && $row->amount == $row->distrubuted_amt && $row->send_to_approval == 0)
+          {
+            if(Auth::user()->can('chalan-sendapproval'))
+            { 
+              $btn = $btn.'<button data-toggle="modal" data-target="#exampleModal" data-whatever="'.$row->id.'" data-chalan=" Challan No.'.$row->chalan_serial_no.' Month Of '.$row->month_name_mar.' Taluka:'.$row->taluka_name.' Classification:'.$row->classification_name_mar.' Total Amount:'.$row->amount.'" class="btn btn-warning btn-sm">Send to Approval</button>';
+            }
+          }*/
+          
+        return $btn;
+    })
+  ->rawColumns(['action'])
+  ->make(true);
+}
+    $data['sixpayentry'] = "";
     return view('Admin.Vetan.sixpay', $data);
   }
   public function store(Request $request)
